83f0d795ca415a310a146ca6dc52d166983e02c8
baozitao <baozitao@gmail.com>
2023-07-16 14:35:32 +0800

    修改了目录

---
+++ dm/dm.sql
@@ -0,0 +1,14 @@
+select username from dba_users;
+--select name from v$tablespace; --查看表空间
+--select name from v$database;  --查看数据库
+
+--select table_name from user_tables;
+
+--select sys_context('USERENV','CURRENT_SCHEMA'); --查看当前模式和用户
+--select TABLE_NAME from DM_TABLE where TABLE_SCHEMA='person';
+--select TABLE_NAME from dba_tables  where owner='PRODUCTION';
+
+select * from dual;
+
+
+
---
+++ dm/无标题1.sql
@@ -0,0 +1,3 @@
+select * from person.address;
+select user from dual;
+select username, default_tablespace from dba_users where username = 'sysdba';
